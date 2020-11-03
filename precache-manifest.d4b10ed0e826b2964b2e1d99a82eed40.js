3a2a46e522b5e5683f976d0fec38f64c71deb160
Volodymyr Mykolaichyk <volodymyr@chefslist.de>
2020-11-04 00:24:52 +0200

    fix: github pages root

---
+++ precache-manifest.d4b10ed0e826b2964b2e1d99a82eed40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6d1293e31f4a994e5788b7efce674f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa740b1d9e0dede39e1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48dfbcd6e01075ab23c5",
+    "url": "/static/js/2.5e9588ca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.5e9588ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aa740b1d9e0dede39e1",
+    "url": "/static/js/main.af5a172f.chunk.js"
+  },
+  {
+    "revision": "ee20722370456157df52",
+    "url": "/static/js/runtime-main.005e237a.js"
+  },
+  {
+    "revision": "e37fc577eb1070764364dd77616c7ea0",
+    "url": "/static/media/1.e37fc577.jpg"
+  }
+]);
